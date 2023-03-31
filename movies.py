736f875cb7eebde197f7c2db99b25d86defe0695
Erik <erik.ernst@muttdata.ai>
2023-03-31 19:17:19 -0300

    feat: rating filter + keywords embeddings

--- movies.py
+++ movies.py
@@ -12,6 +12,19 @@ from utils import seed_everything, streamlit_header_and_footer_setup
 
 torchfy = lambda x: torch.as_tensor(x, dtype=torch.float32)
 
+def get_similarity_combined(target: List[float], candidates_1: List[float], candidates_2: List[float], top_k: int):
+    target = torchfy(target)
+    candidates_1 = torchfy(candidates_1).transpose(0, 1)
+    cos_scores_1 = torch.mm(target, candidates_1)
+    
+    candidates_2 = torchfy(candidates_2).transpose(0, 1)
+    cos_scores_2 = torch.mm(target, candidates_2)
+
+    cos_scores = cos_scores_1 + cos_scores_2
+    scores, indices = torch.topk(cos_scores, k=top_k)
+    similarity_hits = [{'id': idx, 'score': score} for idx, score in zip(indices[0].tolist(), scores[0].tolist())]
+
+    return similarity_hits
 
 def get_similarity(target: List[float], candidates: List[float], top_k: int):
     candidates = torchfy(candidates).transpose(0, 1)
@@ -44,20 +57,19 @@ co = cohere.Client(COHERE_API_KEY)
 def setup():
     PODCAST_FIELDS = [
         "movieId", "id", "imdb_id", "original_title", "title", "overview", "genres", "release_date", "language_code",
-        "lang2idx", "language_name", "embeddings"
+        "lang2idx", "language_name", "embeddings", "keywords_embedding", "vote_average"
     ]
-    movies_df = pd.read_json("./data/the_movies_with_embeddings.json", orient="index")
+    movies_df = pd.read_json("./data/movies_with_keywords_embeddings_31_03_2023.json", orient="index")
     movies_df = movies_df.dropna(subset=['imdb_id'])
     movies_df = movies_df.fillna("")
     movies_df['movieId'] = movies_df.index
     movies_df = movies_df[PODCAST_FIELDS]
-    candidates = np.array(movies_df.embeddings.values.tolist(), dtype=np.float32)
     df_watchlist = pd.read_csv("./data/letterboxd/watchlist.csv")
     df_watched = pd.read_csv("./data/letterboxd/watched.csv")
-    return movies_df, candidates, df_watchlist, df_watched
+    return movies_df, df_watchlist, df_watched
 
 
-movies_df, candidates, df_watchlist, df_watched = setup()
+movies_df, df_watchlist, df_watched = setup()
 
 movies_available_languages = sorted(movies_df.language_name.unique().tolist())
 images_cache = {}
@@ -67,6 +79,7 @@ search_expander = st.expander(label='Search Fields, Expand me!')
 with search_expander:
     'Hello there!'
     limit = st.slider("limit", min_value=1, max_value=100, value=5, step=1)
+    min_imdb_rating = st.slider("min imdb rating", min_value=0.0, max_value=10.0, value=6.5, step=0.1)
     selected_languages = st.multiselect(
         label=f"Desired languages | Number of Unique languages: {len(movies_available_languages)}",
         options=movies_available_languages,
@@ -81,7 +94,8 @@ filters = [selected_languages, only_not_watched, only_in_watchlist]
 retrieve_button = st.button("retrieve! 🧐")
 if query_text or retrieve_button:
     sub_movies_df = movies_df.copy()
-    
+    rating_filter = movies_df["vote_average"] > min_imdb_rating
+    sub_movies_df = sub_movies_df[rating_filter]
     if selected_languages:
         selected_languages_str = "|".join(language for language in selected_languages)
         sub_movies_df = sub_movies_df[sub_movies_df["language_name"].str.contains(selected_languages_str, regex=True)]
@@ -95,7 +109,8 @@ if query_text or retrieve_button:
         watched_filter = sub_movies_df["title"].isin(watched_names) | sub_movies_df["original_title"].isin(watched_names)
         sub_movies_df = sub_movies_df[watched_filter]
     
-    candidates = np.array(sub_movies_df.embeddings.values.tolist(), dtype=np.float32)
+    candidates_content = np.array(sub_movies_df.embeddings.values.tolist(), dtype=np.float32)
+    candidates_keywords = np.array(sub_movies_df.keywords_embedding.values.tolist(), dtype=np.float32)
     
     print(f"Query: {query_text}")
     vectors_to_search = np.array(
@@ -104,7 +119,7 @@ if query_text or retrieve_button:
     )
 
     start_time = time()
-    result = get_similarity(vectors_to_search, candidates=candidates, top_k=limit)
+    result = get_similarity_combined(vectors_to_search, candidates_content, candidates_keywords, top_k=limit)
     print(result)
     end_time = time()
 
