900f2df2e46fef7c9d78aca9e87aa3ea7f3a88a2
Erik <erik.ernst@muttdata.ai>
2023-03-31 19:49:03 -0300

    fix: update requirements + cinamagoer

--- movies.py
+++ movies.py
@@ -37,9 +37,9 @@ def get_similarity(target: List[float], candidates: List[float], top_k: int):
     return similarity_hits
 
 
-import imdb
+from imdb import Cinemagoer
 
-ia = imdb.IMDb()
+ia = Cinemagoer()
 
 seed_everything(3777)
 
